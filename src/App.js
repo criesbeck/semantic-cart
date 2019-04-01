96c34f8c1a8e39136b469fc3750023cb72bc0af3
Chris Riesbeck <chris.riesbeck@gmail.com>
2019-04-01 12:02:28 -0500

    show products, empty cart

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Button, Card, Container, Grid, Image, Segment, Sidebar } from 'semantic-ui-react'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const Product = ({ openCart, product }) => (
+  <Grid.Column>
+  <Card>
+    <Card.Content onClick={openCart}>
+      <Card.Header>{ product.title }</Card.Header>
+      <Image src={require(`./static/data/products/${product.sku}_1.jpg`)} />
+      <Card.Content extra>{ product.style }</Card.Content>
+    </Card.Content>
+  </Card>
+  </Grid.Column>
+);
+
+const ProductList = ({ openCart, products }) => {
+  const items = products.map(product => <Product product={product} openCart={openCart} key={product.sku} />);
+  return (
+    <Grid container stackable stretched columns={4}>
+        {items}
+    </Grid>
+  );
+};
+
+export default ({ products }) => {
+  const [ cartOpen, setCartOpen ] = useState(false);
+  const toggleCart = () => { setCartOpen(!cartOpen); };
+  const openCart = () => { setCartOpen(true); };
+
+  return (
+    <Container text>
+      <Sidebar.Pushable as={Segment}>
+        <Sidebar as={Segment} animation='overlay' icon='labeled'
+                inverted direction='right' vertical width='thin'
+                visible={cartOpen}
+        >
+          The cart is empty
+          <Button onClick={toggleCart}>Close</Button>
+        </Sidebar>
+        <Sidebar.Pusher>
+          <ProductList products={products} openCart={openCart}/>
+        </Sidebar.Pusher>
+      </Sidebar.Pushable>
+    </Container>
+  );
 }
-
-export default App;
